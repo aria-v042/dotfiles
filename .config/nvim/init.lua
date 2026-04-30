6352c07680b0079fbe8b015ad26a48f947eee3d2
aria <redcore_one@proton.me>
2026-04-30 23:37:49 +0100

    nvim: update config, add colorschemes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,30 @@
 -- Set <space> as the leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
-require 'options'
-require 'functions'
+require "options"
+require "functions"
 
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-	local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
-		error('Error cloning lazy.nvim:\n' .. out)
+		error("Error cloning lazy.nvim:\n" .. out)
 	end
 end 
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require('lazy').setup({
+require("lazy").setup({
   spec = {
-    { import = 'plugins' },
+    { import = "plugins" },
   },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
---require 'theme'
+require "colorscheme"
--- .config/nvim/lua/theme.lua
+++
@@ -1,9 +0,0 @@
-
--- ==========================
--- Colorscheme & transparency
--- ==========================
-
-vim.cmd.colorscheme("wildcharm")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
