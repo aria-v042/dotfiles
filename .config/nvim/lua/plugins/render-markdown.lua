439d97cd5b4e8ac46ff94e738a00080e8643c97d
aria <redcore_one@proton.me>
2026-05-17 19:36:20 +0100

    nvim: add markdown-preview plugin and disable render-markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,9 +1,10 @@
--- lua/plugins/render-markdown.lua
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
+		--"echasnovski/mini.icons"
 		"nvim-tree/nvim-web-devicons", -- optional but recommended for file icons
 	},
 	ft = { "markdown" }, -- only load when opening a markdown file
+	enabled = false,
 }
