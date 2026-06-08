3e8fb54da49ac49002fcdddea154e3869f7325da
aria <redcore_one@proton.me>
2026-06-08 22:53:17 +0100

    nvim: add preview-markdown plugin configurations and keybind

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -6,4 +6,17 @@ return {
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
+  config = function()
+	  -- set to 1, nvim will open the preview window after entering the Markdown buffer
+	  -- default: 0
+	  vim.g.mkdp_auto_start = 1
+
+	  -- set to 1, the nvim will auto close current preview window when changing
+	  -- from Markdown buffer to another buffer
+	  -- default: 1
+	  vim.g.mkdp_auto_close = 1
+	  
+	  -- keybinds:
+	  vim.keymap.set("n", "<leader>mp", "<Plug>MarkdownPreviewToggle", { desc = "Toggle Markdown Preview"})
+  end,
 }
