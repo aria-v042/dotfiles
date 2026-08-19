fc948563a276a5561f25e5140c9d19ccf0932831
aria <redcore_one@proton.me>
2026-08-20 00:25:28 +0100

    nvim: temporarily disable lsp configs (commented)

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,23 +1,23 @@
 return {
- 	{
- 		"mason-org/mason.nvim",
- 		opts = {
- 			ui = {
- 			icons = {
- 				package_installed = "✓",
- 				package_pending = "➜",
- 				package_uninstalled = "✗"
- 			}},
- 		},
- 	},
- 	{
- 		"mason-org/mason-lspconfig.nvim",
- 		dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
- 		opts = {
- 			ensure_installed = {
- 				"clangd",
- 			},
- 			automatic_enable = true,
- 		},
- 	},
+ 	--{
+ 	--	"mason-org/mason.nvim",
+ 	--	opts = {
+ 	--		ui = {
+ 	--		icons = {
+ 	--			package_installed = "✓",
+ 	--			package_pending = "➜",
+ 	--			package_uninstalled = "✗"
+ 	--		}},
+ 	--	},
+ 	--},
+ 	--{
+ 	--	"mason-org/mason-lspconfig.nvim",
+ 	--	dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
+ 	--	opts = {
+ 	--		ensure_installed = {
+ 	--			"clangd",
+ 	--		},
+ 	--		automatic_enable = true,
+ 	--	},
+ 	--},
 }
