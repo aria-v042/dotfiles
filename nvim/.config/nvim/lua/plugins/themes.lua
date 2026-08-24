337e716636ef408c3117631d521225b18374405e
aria <redcore_one@proton.me>
2026-08-24 04:30:43 +0100

    nvim: review 'catppuccin/nvim' config

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -4,7 +4,6 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			---@diagnostic disable-next-line: missing-fields
 			require("catppuccin").setup({
 				flavour = "auto", -- latte, frappe, macchiato, mocha
 				background = { -- :h background
@@ -12,45 +11,17 @@ return {
 					dark = "mocha",
 				},
 				transparent_background = true, -- disables setting the background color.
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+				float = {
+					transparent = true, -- enable transparent floating windows
+				},
 				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
 				dim_inactive = {
-					enabled = false, -- dims the background color of inactive window
+					enabled = true, -- dims the background color of inactive window
 					shade = "dark",
 					percentage = 0.15, -- percentage of the shade to apply to the inactive window
 				},
-				no_italic = false, -- Force no italic
-				no_bold = false, -- Force no bold
-				no_underline = false, -- Force no underline
-				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-					comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
-					loops = {},
-					functions = {},
-					keywords = {},
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
-					-- miscs = {}, -- Uncomment to turn off hard-coded styles
-				},
-				color_overrides = {},
-				custom_highlights = {},
-				default_integrations = true,
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					treesitter = true,
-					notify = false,
-					mini = {
-						enabled = true,
-						indentscope_color = "",
-					},
-				},
+				auto_integrations = true,
+				-- For plugins integrations (https://github.com/catppuccin/nvim#integrations)
 			})
 		end,
 	},
